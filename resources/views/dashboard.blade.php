152b2256403bd214f4173a895209182c17889f2e
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-07 10:40:34 -0300

    2 - Criando o layout inicial e configurando o projeto

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Meus horários') }}
+        </h2>
+    </x-slot>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+        
+    </table>
+    
+</x-app-layout>
+
+
