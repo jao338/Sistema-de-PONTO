77a81c8ef4f99a75c6a276f4c5820b806cdd5d45
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-21 12:11:29 -0300

    Ajuste

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,21 +25,23 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="flex justify-between mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Lembra de mim') }}</span>
+                <span class="ml-2 text-sm text-gray-600">{{ __('Lembrar de mim') }}</span>
+
             </label>
+
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="/register">
+                {{ __('Ainda não possuo cadastro') }}
+            </a>
         </div>
 
         <div class="flex items-center justify-between mt-4">
             <div class="flex flex-col items-center">
 
-                {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="/register">
-                    {{ __('Ainda não possuo cadastro') }}
-                </a> --}}
-
                 @if (Route::has('password.request'))
+            
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Esqueci minha senha') }}
                 </a>
